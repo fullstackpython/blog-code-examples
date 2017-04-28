cd6ccd1b0ddf7635c1dda626e38b6c6f497b4405
Matt Makai <matthew.makai@gmail.com>
2017-04-28 06:39:14 -0500

    add new code example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# blog-code-examples
-All code snippets from blog post tutorials on fullstackpython.com.
+# fullstackpython.com blog code examples
+All code snippets from 
+[blog post tutorials](https://www.fullstackpython.com/blog.html) 
+found on [fullstackpython.com](https://www.fullstackpython.com/).
--- send-mms-picture-messages-python/send_mms.py
+++ send-mms-picture-messages-python/send_mms.py
@@ -4,9 +4,14 @@ from twilio.rest import Client
 # the following line needs your Twilio Account SID and Auth Token
 client = Client("ACxxxxxxxxxxxxxx", "zzzzzzzzzzzzz")
 
+# this is the URL to an image file we're going to send in the MMS
+media = "https://raw.githubusercontent.com/mattmakai/fullstackpython.com/master/static/img/logos/f.png"
+
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
+# account to send MMS to any phone number that MMS is available
 client.api.account.messages.create(to="+19732644152",
                                    from_="+12023351278",
-                                   body="Hello from Python!")
+                                   body="MMS via Python? Nice!",
+                                   media_url=media)
+

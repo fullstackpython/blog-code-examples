e4c630fdc8468fa178995d5d30e8c3e142ac8cc7
Matt Makai <matthew.makai@gmail.com>
2017-05-21 21:51:14 -0700

    working on bokeh blog post

---
+++ bar-charts-bokeh-flask-python-3/app.py
@@ -0,0 +1,80 @@
+from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,
+                          Range1d)
+from bokeh.models.glyphs import VBar
+from bokeh.plotting import figure
+from bokeh.charts import Bar
+from bokeh.embed import components
+from bokeh.models.sources import ColumnDataSource
+from flask import Flask, render_template
+
+
+flask = Flask(__name__)
+
+
+@app.route("/<bars_count>/")
+def chart(bars_count):
+    return render_template("chart.html")
+
+
+def hover_tool(fields_dict={}):
+    hover_html = """
+      <div>
+        <span class="hover-tooltip">$x</span>
+      </div>
+      <div>
+        <span class="hover-tooltip">@bugs bugs</span>
+      </div>
+      <div>
+        <span class="hover-tooltip">$@cost{0.00}</span>
+      </div>
+    """
+    return HoverTool(tooltips=hover_html)
+
+
+def create_bar_chart(data, title, x_name, y_name, hover_tool=None,
+                     width=1200, height=300):
+    """
+        Creates a bar chart plot with the exact styling for the centcom
+        dashboard. Pass in data as a dictionary, desired plot title,
+        name of x axis, y axis and the hover tool HTML.
+    """
+    source = ColumnDataSource(data)
+    xdr = FactorRange(factors=data[x_name])
+    ydr = Range1d(start=0,end=max(data[y_name])*1.5)
+
+    tools = []
+    if hover_tool:
+        tools = [hover_tool,]
+
+    plot = figure(title=title, x_range=xdr, y_range=ydr, plot_width=width,
+                  plot_height=height, h_symmetry=False, v_symmetry=False,
+                  min_border=0, toolbar_location="above", tools=tools,
+                  responsive=True, outline_line_color="#666666")
+
+    glyph = VBar(x=x_name, top=y_name, bottom=0, width=.8,
+                 fill_color="#e12127")
+    plot.add_glyph(source, glyph)
+
+    xaxis = LinearAxis()
+    yaxis = LinearAxis()
+
+    plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
+    plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
+    plot.toolbar.logo = None
+    plot.title.text_color = "white"
+    plot.background_fill_alpha = 0.05
+    plot.border_fill_alpha = 0
+    plot.min_border_top = 0
+    plot.min_border_bottom = 0
+    plot.xgrid.grid_line_color = None
+    plot.ygrid.grid_line_color = "#999999"
+    plot.yaxis.axis_label_text_color = "white"
+    plot.yaxis.major_label_text_color = "white"
+    plot.yaxis.axis_label = None
+    plot.ygrid.grid_line_alpha = 0.1
+    plot.xaxis.axis_label_text_color = "white"
+    plot.xaxis.major_label_text_color = "white"
+    plot.xaxis.axis_label = None
+    plot.xaxis.major_label_orientation = .785
+    return plot
+

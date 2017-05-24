d9836fce09341aa0bcc61359f23356f2c9642205
Matt Makai <matthew.makai@gmail.com>
2017-05-24 06:58:55 -0700

    working on bokeh simple app

--- bar-charts-bokeh-flask-python-3/app.py
+++ bar-charts-bokeh-flask-python-3/app.py
@@ -1,3 +1,4 @@
+import random
 from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,
                           Range1d)
 from bokeh.models.glyphs import VBar
@@ -16,11 +17,22 @@ def chart(bars_count):
     if bars_count <= 0:
         bars_count = 1
 
+    data = {"days": [], "bugs": [], "costs": []}
+    for i in range(1, bars_count+1):
+        data['days'].append(i)
+        data['bugs'].append(random.randint(1,100))
+        data['costs'].append(random.uniform(1.00, 1000.00))
+
+    hover = create_hover_tool()
+    plot = create_bar_chart(data, "Bugs found per day", "days",
+                            "bugs", hover)
+    script, div = components(plot)
+
     return render_template("chart.html", bars_count=bars_count,
-                           the_div=None, the_script=None)
+                           the_div=div, the_script=script)
 
 
-def hover_tool(fields_dict={}):
+def create_hover_tool():
     hover_html = """
       <div>
         <span class="hover-tooltip">$x</span>

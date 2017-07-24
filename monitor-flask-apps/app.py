9811f6b8a9db53761590d5840b37f74d512faad2
Matt Makai <matthew.makai@gmail.com>
2017-07-24 16:27:07 -0400

    update example app for flask monitoring

--- monitor-flask-apps/app.py
+++ monitor-flask-apps/app.py
@@ -1,26 +1,37 @@
 import os
 import re
 import rollbar
-from flask import Flask, render_template
+import rollbar.contrib.flask
+from flask import Flask, render_template, Response
+from flask import got_request_exception
 from werkzeug.exceptions import NotFound
 
 
 app = Flask(__name__)
-rollbar.init(os.environ.get('ROLLBAR_SECRET'))
-rollbar.report_message('Rollbar is configured correctly')
-
 MIN_PAGE_NAME_LENGTH = 2
+
+
+@app.before_first_request
+def add_monitoring():
+    rollbar.init(os.environ.get('ROLLBAR_SECRET'))
+    ## delete the next line if you dont want this event anymore
+    rollbar.report_message('Rollbar is configured correctly')
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 
 @app.route("/<string:page>/")
 def show_page(page):
-    valid_length = len(page) >= MIN_PAGE_NAME_LENGTH
-    valid_name = re.match('^[a-z]+$', page.lower()) is not None
-    if valid_length and valid_name:
-        return render_template("{}.html".format(page))
-    else:
-        msg = "Sorry, couldn't find page with name {}".format(page)
-        raise NotFound(msg)
+    try:
+        valid_length = len(page) >= MIN_PAGE_NAME_LENGTH
+        valid_name = re.match('^[a-z]+$', page.lower()) is not None
+        if valid_length and valid_name:
+            return render_template("{}.html".format(page))
+        else:
+            msg = "Sorry, couldn't find page with name {}".format(page)
+            raise NotFound(msg)
+    except:
+        rollbar.report_exc_info()
+        return Response("404 Not Found")
 
 
 if __name__ == "__main__":

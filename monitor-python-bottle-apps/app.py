4361bfa8e01891d64954f9d6045e11bcb2dddc1e
Matt Makai <matthew.makai@gmail.com>
2017-09-29 12:37:56 -0400

    update code latest post

--- monitor-python-bottle-apps/app.py
+++ monitor-python-bottle-apps/app.py
@@ -1,38 +1,42 @@
+import bottle
 import os
 import re
-import rollbar
-import rollbar.contrib.flask
-from flask import Flask, render_template, Response
-from flask import got_request_exception
-from werkzeug.exceptions import NotFound
+from bottle import route, template
+from rollbar.contrib.bottle import RollbarBottleReporter
 
 
-app = Flask(__name__)
-MIN_PAGE_NAME_LENGTH = 2
+TEMPLATE_STRING = """
+<html>
+ <head>
+  <title>Full Stack Python Web App</title>
+ </head>
+ <body>
+  <h1>{{ h1 }}</h1>
+ </body>
+</html>
+"""
+
+MIN_MSG_LENGTH = 2
+ROLLBAR_SECRET = os.environ.get("ROLLBAR_SECRET")
+
+rb_monitor = RollbarBottleReporter(access_token=ROLLBAR_SECRET,
+                                   environment='production')
+bottle.install(rb_monitor)
 
 
-@app.before_first_request
-def add_monitoring():
-    rollbar.init(os.environ.get('ROLLBAR_SECRET'))
-    ## delete the next line if you dont want this event anymore
-    rollbar.report_message('Rollbar is configured correctly')
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-
-
-@app.route("/<string:page>/")
-def show_page(page):
-    try:
-        valid_length = len(page) >= MIN_PAGE_NAME_LENGTH
-        valid_name = re.match('^[a-z]+$', page.lower()) is not None
-        if valid_length and valid_name:
-            return render_template("{}.html".format(page))
-        else:
-            msg = "Sorry, couldn't find page with name {}".format(page)
-            raise NotFound(msg)
-    except:
-        rollbar.report_exc_info()
-        return Response("404 Not Found")
+@route("/<msg>/")
+def show_message(msg):
+    """Display a message if the msg value is greater than 2 characters
+    in the path.
+    """
+    valid_length = len(msg) >= MIN_MSG_LENGTH
+    valid_name = re.match('^[a-z\-]+$', msg.lower()) is not None
+    if valid_length and valid_name:
+        return template(TEMPLATE_STRING, h1=msg)
+    else:
+        error_msg = "Sorry, only alpha characters and hyphens allowed."
+        raise Exception(error_msg)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    bottle.run(host='localhost', port=8080)

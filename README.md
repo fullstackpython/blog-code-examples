82ee0c4897f476609d83e9d137a26f3e078c1131
Matt Makai <matthew.makai@gmail.com>
2017-11-30 14:37:51 -0500

    update script for blog post

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Posts and associated code:
 
 |Post|Code Directory|Notes|
 |---|---|---|
+|First Steps with GitPython|[first-step-gitpython](https://github.com/fullstackpython/blog-code-examples/tree/master/first-steps-gitpython)||
 |[How to Monitor Python Web Applications](https://www.fullstackpython.com/blog/blog/monitor-python-web-applications.html)|[monitor-python-bottle-apps](https://github.com/fullstackpython/blog-code-examples/tree/master/monitor-python-bottle-apps)||
 |[How to Provision Ubuntu 16.04 Linux Servers on Linode](https://www.fullstackpython.com/blog/provision-ubuntu-linux-servers-linode.html)|No code for this post.||
 |[Creating Bar Chart Visuals with Bokeh, Bottle and Python 3](https://www.fullstackpython.com/blog/python-bottle-bokeh-bar-charts.html)|[bar-charts-bokeh-bottle-python-3](https://github.com/fullstackpython/blog-code-examples/tree/master/bar-charts-bokeh-bottle-python-3)||
---
+++ first-steps-gitpython/read_repo.py
@@ -0,0 +1,43 @@
+import os
+from git import Repo
+
+
+COMMITS_TO_PRINT = 5
+
+
+def print_commit(commit):
+    print('----')
+    print(str(commit.hexsha))
+    print("\"{}\" by {} ({})".format(commit.summary,
+                                     commit.author.name,
+                                     commit.author.email))
+    print(str(commit.authored_datetime))
+    print(str("count: {} and size: {}".format(commit.count(),
+                                              commit.size)))
+    print(str(commit.size))
+
+
+def print_repository(repo):
+    print('Repo active branch is {}'.format(repo.active_branch))
+    print('Repo description: {}'.format(repo.description))
+    print('Repo active branch is {}'.format(repo.active_branch))
+    for remote in repo.remotes:
+        print('Remote named "{}" with URL "{}"'.format(remote, remote.url))
+    print('Last commit for repo is {}.'.format(str(repo.head.commit.hexsha)))
+
+
+if __name__ == "__main__":
+    repo_path = os.getenv('GIT_REPO_PATH')
+    # Repo object used to programmatically interact with Git repositories
+    repo = Repo(repo_path)
+    # check that the repository loaded correctly
+    if not repo.bare:
+        print('Repo at {} successfully loaded.'.format(repo_path))
+        print_repository(repo)
+        # create list of commits then print some of them to stdout
+        commits = list(repo.iter_commits('master'))[:COMMITS_TO_PRINT]
+        for commit in commits:
+            print_commit(commit)
+            pass
+    else:
+        print('Could not load repository at {} :('.format(repo_path))

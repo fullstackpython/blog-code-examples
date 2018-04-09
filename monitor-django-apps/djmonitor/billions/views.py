c028dd4366e8279d1a248cf70efcf97f53b85f17
Matt Makai <matthew.makai@gmail.com>
2018-04-09 09:23:28 -0400

    add code from flight yesterday new urls and view

--- monitor-django-apps/djmonitor/billions/views.py
+++ monitor-django-apps/djmonitor/billions/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def theyare(request):
+    return HttpResponse("billions")
--- monitor-django-apps/djmonitor/djmonitor/urls.py
+++ monitor-django-apps/djmonitor/djmonitor/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path(r'^billions/', include('billions.urls', namespace='billions')),
+    path('billions/', include('billions.urls')),
     path('admin/', admin.site.urls),
 ]

ff39faff7f95f726c6b0124d5d12366f1e61d420
Matt Makai <matthew.makai@gmail.com>
2018-05-26 17:55:08 -0400

    add new blog post on maps in django

--- maps-django-mapbox/djmaps/maps/views.py
+++ maps-django-mapbox/djmaps/maps/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def default_map(request):
+    # TODO: move this token to Django settings from an environment variable
+    # found in the Mapbox account settings and getting started instructions
+    # see https://www.mapbox.com/account/ under the "Access tokens" section
+    mapbox_access_token = 'pk.your_mapbox_access_token'
+    return render(request, 'default.html',
+                  { 'mapbox_access_token': mapbox_access_token })

d9b4b856487936a26f40817baf5c32dd292eb11b
Matt Makai <matthew.makai@gmail.com>
2020-07-19 13:10:51 -0400

    add new post for tracking user visits in django

--- render-markdown-django-3-0-django-markdown-view/step2/djmdview/rendermarkdown/urls.py
+++ render-markdown-django-3-0-django-markdown-view/step2/djmdview/rendermarkdown/urls.py
@@ -1,11 +1,14 @@
 # djmdview/rendermarkdown/urls.py
 from django.conf.urls import url
-from markdown_view import MarkdownView
+from django.urls import path
+
+from markdown_view.views import MarkdownView
 from . import views
 
+
 urlpatterns = [
-    url(r'', views.rendermarkdown_index, name="index"),
     path('readme/',
          MarkdownView.as_view(file_name='rendermarkdown/README.md'),
          name="readme"),
+    url(r'', views.rendermarkdown_index, name="index"),
 ]
